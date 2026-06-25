46909ae77f73d0ebae695376e16695add28dea56
Edvaldo-jb-Junior <juniorb2325@gmail.com>
2026-06-25 16:31:13 -0300

    Project start

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { number } from "yup";
+import app from "./app"
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = Number(process.env.PORT);
+
+const startServer = async () => {
+    try {
+        await app.listen({ port: PORT }).then(() => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+startServer();
